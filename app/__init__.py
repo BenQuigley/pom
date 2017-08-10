234966bd7c9b809e750ea06f6f112498e5670b0d
Ben Quigley <benquigleyconsulting@gmail.com>
2017-08-10 13:37:12 -0400

    moving db creation to models.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
-from app.models import User, Poem
-
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
