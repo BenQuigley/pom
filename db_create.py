a54df47a70b375eb9de613b5ba83dab3a8019ba3
Ben Quigley <benquigleyconsulting@gmail.com>
2017-08-11 10:49:46 -0400

    updated email address to the one with my gravatar

--- db_create.py
+++ db_create.py
@@ -28,7 +28,7 @@ navigate = "What tempestuous destiny rushes at them\n" \
            "In the bed sheets together – recalling\n" \
            "The Polaroid memories of their childhoods, but reflecting\n" \
            "How the film for a Polaroid just isn't made anymore."
-user = User(nickname="iroh", password='meow', email='benquigleyconsulting@gmail.com')
+user = User(nickname="iroh", password='meow', email='donnerblues@gmail.com')
 poem = Poem(name="They Navigate by Constellation", body=navigate, user_id=user.id)
 
 db.session.add(user)
