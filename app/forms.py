417ac29a2a181edc238ddd9cd75dec3f685a11a9
Ben Quigley <benquigleyconsulting@gmail.com>
2017-08-12 09:00:21 -0400

    added edit poem form

--- app/forms.py
+++ app/forms.py
@@ -9,8 +9,11 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField('Remember me', default=False)
 
 class CreateAccountForm(FlaskForm):
-    username = StringField('Username', [validators.DataRequired("Username is required.")])
-    email = StringField('Email', [validators.DataRequired("Email is required."), validators.Email(message="Email must be an email.")])
+    username = StringField('Username', [validators.DataRequired("Username is required."),
+                                        validators.Length(min=4, max=16)])
+    email = StringField('Email', [validators.DataRequired("Email is required."),
+                                  validators.Email(message="Email must be an email."),
+                                  validators.Length(min=0, max=120)])
     password = PasswordField('Password', [validators.DataRequired("Password is required.")])
     password_confirm = PasswordField('Password confirm',
                                      [validators.DataRequired("Password confirmation is required."),
@@ -20,3 +23,7 @@ class CreateAccountForm(FlaskForm):
 class EditProfileForm(FlaskForm):
     nickname = StringField('nickname', [validators.DataRequired()])
     about_me = TextAreaField('about_me', [validators.Length(min=0, max=140)])
+
+class EditPoemForm(FlaskForm):
+    body = TextAreaField('body', [validators.Length(min=0, max=140)])
+    about_me = TextAreaField('about_me', [validators.Length(min=0, max=1400)])
