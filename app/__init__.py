860e56379af441f0b148978c201ed48271b0db07
Ben Quigley <benquigleyconsulting@gmail.com>
2017-08-13 11:55:14 -0400

    got rid of flask-bootstrap and added bootstrap via flask-bower

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,12 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_bootstrap import Bootstrap
+from flask_bower import Bower
 
 from config import basedir, HEROKU
 
 app = Flask(__name__)
-Bootstrap(app)
+Bower(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
