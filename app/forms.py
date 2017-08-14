82a426202a6aea02cabe259050cba27f81e55d1c
Ben Quigley <benquigleyconsulting@gmail.com>
2017-08-14 15:07:57 -0400

    updated to support writing and editing poems

--- app/forms.py
+++ app/forms.py
@@ -4,26 +4,26 @@ from wtforms import validators
 
 class LoginForm(FlaskForm):
 
-    username = StringField('Username', [validators.DataRequired("Username is required.")])
-    password = PasswordField('Password', [validators.DataRequired("Password is required.")])
-    remember_me = BooleanField('Remember me', default=False)
+    username = StringField('nickname', [validators.DataRequired("Username is required.")])
+    password = PasswordField('password', [validators.DataRequired("Password is required.")])
+    remember_me = BooleanField('remember me', default=False)
 
 class CreateAccountForm(FlaskForm):
-    username = StringField('Username', [validators.DataRequired("Username is required."),
+    username = StringField('nickname', [validators.DataRequired("Username is required."),
                                         validators.Length(min=4, max=16)])
-    email = StringField('Email', [validators.DataRequired("Email is required."),
+    email = StringField('email', [validators.DataRequired("Email is required."),
                                   validators.Email(message="Email must be an email."),
                                   validators.Length(min=0, max=120)])
-    password = PasswordField('Password', [validators.DataRequired("Password is required.")])
-    password_confirm = PasswordField('Password confirm',
+    password = PasswordField('password', [validators.DataRequired("Password is required.")])
+    password_confirm = PasswordField('password confirmation',
                                      [validators.DataRequired("Password confirmation is required."),
                                       validators.EqualTo('password', message="Passwords must match.")])
-    remember_me = BooleanField('Remember me', default=False)
+    remember_me = BooleanField('remember me', default=False)
 
 class EditProfileForm(FlaskForm):
     nickname = StringField('nickname', [validators.DataRequired()])
-    about_me = TextAreaField('about_me', [validators.Length(min=0, max=140)])
+    about_me = TextAreaField('about me', [validators.Length(min=0, max=140)])
 
 class EditPoemForm(FlaskForm):
-    body = TextAreaField('body', [validators.Length(min=0, max=140)])
-    about_me = TextAreaField('about_me', [validators.Length(min=0, max=1400)])
+    name = StringField('name', [validators.DataRequired()])
+    body = TextAreaField('body', [validators.Length(min=0, max=1400)])
--- app/views.py
+++ app/views.py
@@ -6,13 +6,21 @@ from flask_login import login_user, current_user, logout_user, login_required
 
 from app import app, db
 
-from .forms import LoginForm, CreateAccountForm, EditProfileForm
+from .forms import LoginForm, CreateAccountForm, EditProfileForm, EditPoemForm
 from .models import User, Poem
 
 def is_safe_url(target):
     ref_url = urlparse(request.host_url)
     test_url = urlparse(urljoin(request.host_url, target))
     return test_url.scheme in ('http', 'https') and ref_url.netloc == test_url.netloc
+
+def flash_errors(form):
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash(u"Error in the %s field - %s" % (
+                getattr(form, field).label.text,
+                error
+            ))
 
 @app.before_request
 def before_request():
@@ -25,7 +33,7 @@ def before_request():
 @app.route('/')
 @app.route('/index')
 def index():
-    poems = Poem.query.order_by(Poem.timestamp).all()
+    poems = Poem.query.order_by(Poem.timestamp.desc()).all()
     return render_template('index.html', poems=poems, verbose=True)
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -70,7 +78,9 @@ def create():
             if not is_safe_url(next):
                 return abort(400)
             return redirect(next or url_for('index'))
-    return render_template('create.html',
+    else:
+        flash_errors(form)
+        return render_template('create.html',
                            page_title='Create Account',
                            form=form)
 
@@ -91,21 +101,54 @@ def user(nickname):
         return redirect(url_for('index'))
     return render_template('user.html', user=user)
 
-@app.route('/editpoem/<poem_id>')
-def edit_poem(poem_id):
-    poem = Poem.query.filter_by(id=poem_id).first()
-    if poem == None:
-        flash('Poem %s not found.' % poem_id)
-        return redirect(url_for('index'))
-    return render_template('editPoem.html', poem=poem, verbose=True, poem_main=True)
-
 @app.route('/poem/<poem_id>')
 def poem(poem_id):
     poem = Poem.query.filter_by(id=poem_id).first()
-    if poem == None:
+    if poem is None:
         flash('Poem %s not found.' % poem_id)
         return redirect(url_for('index'))
     return render_template('viewPoem.html', poem=poem, verbose=True, poem_main=True)
+
+@app.route('/editpoem/<poem_id>', methods=['GET', 'POST'])
+def edit_poem(poem_id):
+    form = EditPoemForm()
+    poem = Poem.query.filter_by(id=poem_id).first()
+    if poem is None:
+        flash('Poem %s not found.' % poem_id)
+        return redirect(url_for('index'))
+    if form.validate_on_submit() and g.user == poem.author:
+        if 'delete' in request.form:
+            db.session.delete(poem)
+            db.session.commit()
+            flash('"%s" has been deleted.' % poem.name)
+            return redirect(url_for('index'))
+        else:
+            poem.name = form.name.data
+            poem.body = form.body.data
+            db.session.add(poem)
+            db.session.commit()
+            flash('Your changes have been saved.')
+            return redirect(url_for('poem', poem_id=poem.id))
+    else:
+        flash_errors(form)
+        form.name.data = poem.name
+        form.body.data = poem.body
+        return render_template('editPoem.html', form=form, poem=poem)
+
+@app.route('/newpoem', methods=['GET', 'POST'])
+def new_poem():
+    form = EditPoemForm()
+    if form.validate_on_submit():
+        p = Poem(name = form.name.data,
+                 body = form.body.data,
+                 user_id = g.user.id)
+        db.session.add(p)
+        db.session.commit()
+        flash('Your poem has been published!')
+        return redirect(url_for('poem', poem_id=p.id))
+    else:
+        flash_errors(form)
+        return render_template('editPoem.html', form=form)
 
 @app.route('/user/edit', methods=['GET', 'POST'])
 @login_required
@@ -117,8 +160,9 @@ def edit_profile():
         db.session.add(g.user)
         db.session.commit()
         flash('Your changes have been saved.')
-        return redirect(url_for('/profile/edit'))
+        return redirect(url_for('edit_profile'))
     else:
+        flash_errors(form)
         form.nickname.data = g.user.nickname
         form.about_me.data = g.user.about_me
-    return render_template('editUser.html', form=form, user=g.user)
+        return render_template('editUser.html', form=form, user=g.user)
