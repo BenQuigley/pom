660334190cea0b0c91501257485b02f67bf7511d
Ben Quigley <benquigleyconsulting@gmail.com>
2017-08-16 15:54:34 -0400

    update config.py to support heroku db

--- config.py
+++ config.py
@@ -14,5 +14,10 @@ else:
 WTF_CSRF_ENABLED = True
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
